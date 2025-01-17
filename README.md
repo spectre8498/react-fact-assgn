89cfe0281f184ba7ea8be61dc40f85ea88719a53
Spectre <ambulgekars@gmail.com>
2025-01-17 12:20:28 +0530

    Initial project setup commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# react-fact-assgn
---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+    return (
+        <div className="App">
+            <div className="app-header-container">
+                <p className="app-header-text">React FACT Assignment</p>
+            </div>
+            <div>
+
+            </div>
+        </div>
+    );
+}
+
+export default App;
